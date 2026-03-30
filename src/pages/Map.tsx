c197b334e8468a933f0b175105aaf005ddbec1f2
Uedson Reis <uedsonreis@gmail.com>
2026-03-30 20:27:00 -0300

    WIP

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import MapView, { LongPressEvent } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { Alert, StyleSheet, View } from 'react-native';
 
 export default function MapPage() {
+
+    const [location, setLocation] = React.useState<Location.LocationObject>();
+
+    React.useEffect(() => {
+
+        Location.requestForegroundPermissionsAsync().then((response: Location.LocationPermissionResponse) => {
+            if (response.status === 'granted') {
+                Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Highest }).then(location => {
+                    setLocation(location);
+                });
+            } else {
+                Alert.alert('Permissão negada', 'Permissão para acessar a localização foi negada');
+            }
+        });
+
+    }, []);
+
+    function goToCreatePlace(event: LongPressEvent) {
+        Alert.alert('Long Press', 'Localização: ' + JSON.stringify(event.nativeEvent.coordinate));
+    }
+
     return (
         <View style={styles.container}>
             <MapView
                 style={styles.map}
                 showsUserLocation={true}
                 zoomControlEnabled={true}
-                camera={{
-                    center: { latitude: -12.9892667, longitude: -38.4545611 },
+                camera={location && {
+                    center: location?.coords,
                     heading: 0, pitch: 0, zoom: 15
                 }}
+                onLongPress={goToCreatePlace}
             />
         </View>
     );
